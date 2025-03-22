7c57902be133ba3eff42d49381b87ef73865a1b5
Piyu0423 <piumikamadubashini5@gmail.com>
2025-03-23 01:07:09 +0800

    Add project files.

---
+++ API_Demo/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_Demo.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { 
+        
+        }
+        public DbSet<Pokemon> Pokemon { get; set; }
+    }
+}
